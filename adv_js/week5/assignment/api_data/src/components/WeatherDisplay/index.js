4dd4cfe4aa8cb4b3a52cbb8b7bfceeaab1e907c8
hayleegraham <graham.haylee@yahoo.com>
2023-09-24 19:59:49 -0700

    added few clouds img

--- adv_js/week5/assignment/api_data/src/components/WeatherDisplay/index.js
+++ adv_js/week5/assignment/api_data/src/components/WeatherDisplay/index.js
@@ -5,6 +5,19 @@ const WeatherDisplay = () => {
   const [apiData, setApiData] = useState({});
   const [weatherData, setWeatherData] = useState({ description: "" });
 
+  const weatherDesc = {
+    "smoke": "https://video-images.vice.com/videos/5acd04aaf1cdb35440081cc1/lede/1523982434316-smokeables_2018_chip_can_bong-clean.jpeg",
+    "few clouds": "https://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill,d_placeholder_gq.png,fl_progressive,g_face,h_1080,q_80,w_1920/v1491952193/gq_blowing-smoke-with-a-vape-god.jpg"
+}
+  const ifExists = (desc)=> {
+    
+    const weatherKeys = Object.keys(weatherDesc)
+    const exists = weatherKeys.some((key)=>{
+        return key == desc
+    })
+    return exists
+    
+  }
   useEffect(() => {
     const requestData = async () => {
       const response = await fetch("https://mm214.com/demo.php")
@@ -27,8 +40,8 @@ const WeatherDisplay = () => {
           <h1>{`${apiData.name} Weather`}</h1>
           <p>{`Weather Description: ${weatherData.description}`}</p>
 
-          {weatherData.description == "smoke" && (
-            <img src="https://video-images.vice.com/videos/5acd04aaf1cdb35440081cc1/lede/1523982434316-smokeables_2018_chip_can_bong-clean.jpeg"></img>
+          {ifExists(weatherData.description) && (
+            <img src={weatherDesc[weatherData.description]}></img>
           )}
           <p>{`Temp: ${apiData.main.temp}`}</p>
           <div className={styles.humidity}>{`Humidity: ${apiData.main.humidity}%`}<div style={{"--bg-width": `${apiData.main.humidity}%`}} className={styles.humidityBar}><div></div></div></div>
