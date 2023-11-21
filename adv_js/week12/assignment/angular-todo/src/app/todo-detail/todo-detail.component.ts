4ea5eb9d11fb1cc02f28796ef18fd34d80bbb564
hayleegraham <graham.haylee@yahoo.com>
2023-11-20 21:15:57 -0800

    todo app

---
+++ adv_js/week12/assignment/angular-todo/src/app/todo-detail/todo-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-detail',
+  templateUrl: './todo-detail.component.html',
+  styleUrls: ['./todo-detail.component.scss'],
+})
+export class TodoDetailComponent implements OnInit {
+  @Input() todo!: Todo;
+  constructor() {}
+  ngOnInit() {}
+}
